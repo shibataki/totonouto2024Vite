acc53628836dfb978d26745d2c92e02ba6e87302
shibataki <shiba.sbtk@gmail.com>
2024-07-24 19:00:20 +0900

    add how to book

--- vite.config.ts
+++ vite.config.ts
@@ -22,6 +22,7 @@ export default defineConfig({
 				article2: resolve(__dirname, 'article2/index.html'),
 				article3: resolve(__dirname, 'article3/index.html'),
 				article4: resolve(__dirname, 'article4/index.html'),
+				article5: resolve(__dirname, 'article5/index.html'),
 			},
 		},
 	},
